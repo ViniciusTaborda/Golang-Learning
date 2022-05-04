44273b388ffa1a23f9da00fa7115b48ef25aaa8b
ViniciusTaborda <vinieducosta+1@outlook.com>
2022-05-04 08:56:54 -0300

    Finished language concepts!

--- DB/Basic CRUD/main.go
+++ DB/Basic CRUD/main.go
@@ -15,6 +15,8 @@ func main() {
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
 	router.HandleFunc("/users", server.GetUsers).Methods(http.MethodGet)
 	router.HandleFunc("/users/{id}", server.GetUser).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.UpdateUser).Methods(http.MethodPut)
+	router.HandleFunc("/users/{id}", server.DeleteUser).Methods(http.MethodDelete)
 
 	fmt.Println("Listening on port 5000.")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- DB/Basic CRUD/server/server.go
+++ DB/Basic CRUD/server/server.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type user struct {
@@ -24,6 +27,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	if err = json.Unmarshal(requestBody, &user); err != nil {
 		w.Write([]byte("Failed to create user struct."))
+		return
 	}
 
 	fmt.Println(user)
@@ -93,5 +97,116 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	ID, err := strconv.ParseUint(params["id"], 10, 32)
+	if err != nil {
+		w.Write([]byte("Failed to get user ID."))
+		return
+	}
+
+	db, err := database.Connect()
+
+	if err != nil {
+		w.Write([]byte("Failed to connect with database."))
+		return
+	}
+	defer db.Close()
+
+	row, err := db.Query("SELECT * FROM USERS WHERE ID= ?", ID)
+	if err != nil {
+		w.Write([]byte("Failed to fetch user."))
+		return
+	}
+
+	var user user
+	if row.Next() {
+		if err := row.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			w.Write([]byte("Failed to scan user."))
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		w.Write([]byte("Failed to json enconde user."))
+		return
+	}
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	ID, err := strconv.ParseUint(params["id"], 10, 32)
+	if err != nil {
+		w.Write([]byte("Failed to get user ID."))
+		return
+	}
+
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.Write([]byte("Failed to get user ID."))
+		return
+	}
+
+	var user user
+	if err := json.Unmarshal(requestBody, &user); err != nil {
+		w.Write([]byte("Failed to create user struct."))
+		return
+	}
+
+	db, err := database.Connect()
+
+	if err != nil {
+		w.Write([]byte("Failed to connect with database."))
+		return
+	}
+	defer db.Close()
+
+	statement, err := db.Prepare("UPDATE USER SET NAME = ?, EMAIL = ? WHER ID = ?")
+	if err != nil {
+		w.Write([]byte("Failed to connect with database."))
+		return
+	}
+	defer statement.Close()
+
+	if _, err := statement.Exec(user.Name, user.Email, ID); err != nil {
+		w.Write([]byte("Failed to update user."))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	ID, err := strconv.ParseUint(params["id"], 10, 32)
+	if err != nil {
+		w.Write([]byte("Failed to get user ID."))
+		return
+	}
+
+	db, err := database.Connect()
+
+	if err != nil {
+		w.Write([]byte("Failed to connect with database."))
+		return
+	}
+	defer db.Close()
+
+	statement, err := db.Prepare("DELETE FROM USERS WHERE ID = ?")
+	if err != nil {
+		w.Write([]byte("Failed to connect with database."))
+		return
+	}
+	defer statement.Close()
+
+	if _, err := statement.Exec(ID); err != nil {
+		w.Write([]byte("Failed to delete user"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
